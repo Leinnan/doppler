4b606801ae6c5e7f95e9ff8a38b3d51d035044b1
Piotr <siuszko@zoho.com>
2020-08-18 18:20:22 +0200

    Shader

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use std::mem;
 use std::os::raw::c_void;
 
 mod consts;
+mod shader;
 
 pub fn main() {
     // glfw: initialize and configure
@@ -36,92 +37,72 @@ pub fn main() {
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
 
-    let (shaderProgram, VAO) = unsafe {
-        let vertexShader =  gl::CreateShader(gl::VERTEX_SHADER);
-        let c_str_vertex = CString::new(consts::VERTEX_SHADER_SRC.as_bytes()).unwrap();
-        gl::ShaderSource(vertexShader, 1, &c_str_vertex.as_ptr(), ptr::null());
-        gl::CompileShader(vertexShader);
-
-
-        // check for shader compile errors
-        let mut success = gl::FALSE as GLint;
-        let mut infoLog = Vec::with_capacity(512);
-        infoLog.set_len(512 - 1); // subtract 1 to skip the trailing null character
-        gl::GetShaderiv(vertexShader, gl::COMPILE_STATUS, &mut success);
-        if success != gl::TRUE as GLint {
-            gl::GetShaderInfoLog(vertexShader, 512, ptr::null_mut(), infoLog.as_mut_ptr() as *mut GLchar);
-            println!("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n{}", str::from_utf8(&infoLog).unwrap());
-        }
-
-        let fragmentShader = gl::CreateShader(gl::FRAGMENT_SHADER);
-        let c_str_fragment = CString::new(consts::FRAGMENT_SHADER_SRC.as_bytes()).unwrap();
-        gl::ShaderSource(fragmentShader,1, &c_str_fragment.as_ptr(), ptr::null());
-        gl::CompileShader(fragmentShader);
-
-
-        // check for shader compile errors
-        gl::GetShaderiv(fragmentShader, gl::COMPILE_STATUS, &mut success);
-        if success != gl::TRUE as GLint {
-            gl::GetShaderInfoLog(fragmentShader, 512, ptr::null_mut(), infoLog.as_mut_ptr() as *mut GLchar);
-            println!("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n{}", str::from_utf8(&infoLog).unwrap());
-        }
-
-        let shaderProgram = gl::CreateProgram();
-        gl::AttachShader(shaderProgram, vertexShader);
-        gl::AttachShader(shaderProgram, fragmentShader);
-        gl::LinkProgram(shaderProgram);
-        
-        // check for linking errors
-        gl::GetProgramiv(shaderProgram, gl::LINK_STATUS, &mut success);
-        if success != gl::TRUE as GLint {
-            gl::GetProgramInfoLog(shaderProgram, 512, ptr::null_mut(), infoLog.as_mut_ptr() as *mut GLchar);
-            println!("ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n{}", str::from_utf8(&infoLog).unwrap());
-        }
-        gl::DeleteShader(vertexShader);
-        gl::DeleteShader(fragmentShader);
-
+    let (shaderObject, VAO) = unsafe {
+        let vShaderCode = CString::new(consts::VERTEX_SHADER_SRC.as_bytes()).unwrap();
+        let fShaderCode = CString::new(consts::FRAGMENT_SHADER_SRC.as_bytes()).unwrap();
+        let shader = crate::shader::Shader::new(vShaderCode, fShaderCode);
         // set up vertex data (and buffer(s)) and configure vertex attributes
         // ------------------------------------------------------------------
         // HINT: type annotation is crucial since default for float literals is f64
-        let vertices: [f32; 9] = [
-            -0.5, -0.5, 0.0, // left
-             0.5, -0.5, 0.0, // right
-             0.0,  0.5, 0.0  // top
+        let vertices: [f32; 12] = [
+             0.5,  0.5, 0.0,  // top right
+             0.5, -0.5, 0.0,  // bottom right
+            -0.5, -0.5, 0.0,  // bottom left
+            -0.5,  0.5, 0.0   // top left
         ];
-        let (mut VBO, mut VAO) = (0, 0);
+        let indices = [ // note that we start from 0!
+            0, 1, 3,  // first Triangle
+            1, 2, 3   // second Triangle
+        ];
+
+        let (mut VBO, mut VAO, mut EBO) = (0, 0, 0);
         gl::GenVertexArrays(1, &mut VAO);
         gl::GenBuffers(1, &mut VBO);
+        gl::GenBuffers(1, &mut EBO);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
         gl::BindVertexArray(VAO);
 
         gl::BindBuffer(gl::ARRAY_BUFFER, VBO);
         gl::BufferData(gl::ARRAY_BUFFER,
-                        (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
-                        &vertices[0] as *const f32 as *const c_void,
-                        gl::STATIC_DRAW);
+                       (vertices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                       &vertices[0] as *const f32 as *const c_void,
+                       gl::STATIC_DRAW);
 
-        gl::VertexAttribPointer(0,3,gl::FLOAT, gl::FALSE, 3 * mem::size_of::<GLfloat>() as GLsizei, ptr::null());
+        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, EBO);
+        gl::BufferData(gl::ELEMENT_ARRAY_BUFFER,
+                       (indices.len() * mem::size_of::<GLfloat>()) as GLsizeiptr,
+                       &indices[0] as *const i32 as *const c_void,
+                       gl::STATIC_DRAW);
+
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 3 * mem::size_of::<GLfloat>() as GLsizei, ptr::null());
         gl::EnableVertexAttribArray(0);
+
+        // note that this is allowed, the call to gl::VertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
         gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+
+        // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+        // gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, 0);
+
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        gl::BindVertexArray(0);
 
         // uncomment this call to draw in wireframe polygons.
         // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
 
-        (shaderProgram, VAO)
+        (shader, VAO)
     };
 
     // render loop
     // -----------
-    let mut color_r = 0.0;
-    let mut color_g = 0.0;
-    let mut color_b = 0.0;
+    let  color_r = 0.3;
+    let  color_g = 0.3;
+    let  color_b = 0.6;
     while !window.should_close() {
         // events
         // -----
         process_events(&mut window, &events);
 
-        color_r = (color_r + 0.03) % 1.0;
-        color_g = (color_g + 0.01) % 0.6;
-        color_b = (color_b + 0.02) % 1.0;
         // render
         // ------
         unsafe {
@@ -129,9 +110,9 @@ pub fn main() {
             gl::Clear(gl::COLOR_BUFFER_BIT);
 
             // draw our first triangle
-            gl::UseProgram(shaderProgram);
+            shaderObject.useProgram();
             gl::BindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, ptr::null());
         }
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
